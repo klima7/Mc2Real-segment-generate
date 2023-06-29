b76710ee9c0d34121679a3a9230447eef1d9a703
klima7 <github@mail.ukasz.com>
2023-06-29 20:59:35 +0200

    Add using random images to app

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import cv2
 import tempfile
-import numpy as np
+from glob import glob
 from pathlib import Path
+import random
+
+import cv2
+import numpy as np
 
 import streamlit as st
 
@@ -29,16 +32,27 @@ def preprocess_image(mc_image):
     return mc_image.astype(np.float32) / 255
 
 
+def get_random_image():
+    paths = list(glob('sample_images/*.png'))
+    path = random.choice(paths)
+    image = cv2.imread(str(path))[..., ::-1]
+    return image
+
+
 st.title('Minecraft to real converter')
 
-mc_file = st.file_uploader('Minecraft image')
-mc_image = get_uploaded_image(mc_file)
+with st.expander('Input Minecraft image', expanded=True):
 
-if mc_image is not None:
-    st.image(mc_image)
+    mc_file = st.file_uploader('Upload')
 
-if st.button('Convert'):
-    model = get_model()
-    mc_image_pre = preprocess_image(mc_image)
-    real_image = model(mc_image_pre)
-    st.image(real_image)
+    mc_image = get_uploaded_image(mc_file) if mc_file else get_random_image()
+    
+    if st.button('Random'):
+        mc_image = get_random_image()
+
+st.image(mc_image)
+
+model = get_model()
+mc_image_pre = preprocess_image(mc_image)
+real_image = model(mc_image_pre)
+st.image(real_image)
